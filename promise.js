f15f3559134abc72586074695fb0494204bbea73
anisa2472 <anisa.psari24@gmail.com>
2022-09-18 13:20:25 +0700

    Membuat fungsi promiseOutput

--- promise.js
+++ promise.js
@@ -1,7 +1,31 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = (emosi) => {
+  return new Promise((resolve, reject) => {
+    if (emosi !== 'marah' && emosi !== 'tidak marah') {
+      reject('Maaf, emosi tidak ditemukan');
+    }
+    else {
+      let jumlahEmosi = 0;
+      promiseTheaterIXX().then(value => {
+        value.forEach(element => {
+          if (element.hasil === emosi) {
+            jumlahEmosi += 1;
+          };
+        });
+      });
+      promiseTheaterVGC().then(value => {
+        value.forEach(element => {
+          if (element.hasil === emosi) {
+            jumlahEmosi += 1;
+          };
+        });
+        resolve(jumlahEmosi);
+      });
+    };
+  });
+};
 
 module.exports = {
   promiseOutput,
